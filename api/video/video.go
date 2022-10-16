9c6c6be7a526fa0536b83b78932b45e5372ad09d
qvbilam <qvbilam@163.com>
2022-10-16 23:56:06 +0800

    video

--- api/video/video.go
+++ api/video/video.go
@@ -34,32 +34,8 @@ func List(ctx *gin.Context) {
 		return
 	}
 
-	fmt.Println("排序请求: ", request.Sort)
 	client := global.VideoServerClient
-	rsp, err := client.Get(context.Background(), &proto.SearchVideoRequest{
-		UserId:           request.UserId,
-		RegionId:         request.RegionId,
-		CategoryId:       request.CategoryId,
-		Keyword:          request.Keyword,
-		Sort:             request.Sort,
-		IsRecommend:      request.IsRecommend,
-		IsNew:            request.IsNew,
-		IsHot:            request.IsHot,
-		IsEnd:            request.IsEnd,
-		IsVisible:        request.IsVisible,
-		TotalCountMin:    request.TotalCountMin,
-		TotalCountMax:    request.TotalCountMax,
-		FavoriteCountMin: request.FavoriteCountMin,
-		FavoriteCountMax: request.FavoriteCountMax,
-		LikeCountMin:     request.LikeCountMin,
-		LikeCountMax:     request.LikeCountMax,
-		PlayCountMin:     request.PlayCountMin,
-		PlayCountMax:     request.PlayCountMax,
-		BarrageCountMin:  request.BarrageCountMin,
-		BarrageCountMax:  request.BarrageCountMax,
-		Page:             request.Page,
-		PerPage:          request.PerPage,
-	})
+	rsp, err := client.Get(context.Background(), HandleSearchRequest(&request))
 
 	if err != nil {
 		api.HandleGrpcErrorToHttp(ctx, err)
@@ -67,4 +43,98 @@ func List(ctx *gin.Context) {
 	}
 	fmt.Printf("%+v\n", request)
 	ctx.JSON(http.StatusOK, rsp)
+}
+
+func HandleSearchRequest(request *validate.VideoList) *proto.SearchVideoRequest {
+	r := &proto.SearchVideoRequest{}
+
+	if request.UserId > 0 {
+		r.UserId = request.UserId
+	}
+
+	if request.RegionId > 0 {
+		r.RegionId = request.RegionId
+	}
+
+	if request.CategoryId > 0 {
+		r.CategoryId = request.CategoryId
+	}
+
+	if request.Keyword != "" {
+		r.Keyword = request.Keyword
+	}
+
+	if request.Sort != "" {
+		r.Sort = request.Sort
+	}
+
+	if &request.IsRecommend != nil {
+		r.IsRecommend = request.IsRecommend
+	}
+
+	if &request.IsNew != nil {
+		r.IsNew = request.IsNew
+	}
+
+	if &request.IsHot != nil {
+		r.IsHot = request.IsHot
+	}
+
+	if &request.IsEnd != nil {
+		r.IsEnd = request.IsEnd
+	}
+
+	if &request.IsVisible != nil {
+		r.IsVisible = request.IsVisible
+	}
+
+	if request.TotalCountMin > 0 {
+		r.TotalCountMin = request.TotalCountMin
+	}
+
+	if request.TotalCountMax > 0 {
+		r.TotalCountMax = request.TotalCountMax
+	}
+
+	if request.FavoriteCountMin > 0 {
+		r.FavoriteCountMin = request.FavoriteCountMin
+	}
+
+	if request.FavoriteCountMax > 0 {
+		r.FavoriteCountMax = request.FavoriteCountMax
+	}
+
+	if request.LikeCountMin > 0 {
+		r.LikeCountMin = request.LikeCountMin
+	}
+
+	if request.LikeCountMax > 0 {
+		r.LikeCountMax = request.LikeCountMax
+	}
+
+	if request.PlayCountMin > 0 {
+		r.PlayCountMin = request.PlayCountMin
+	}
+
+	if request.PlayCountMax > 0 {
+		r.PlayCountMax = request.PlayCountMax
+	}
+
+	if request.BarrageCountMin > 0 {
+		r.BarrageCountMin = request.BarrageCountMin
+	}
+
+	if request.BarrageCountMax > 0 {
+		r.BarrageCountMax = request.BarrageCountMax
+	}
+
+	if request.Page > 0 {
+		r.Page = request.Page
+	}
+
+	if request.PlayCountMax > 0 {
+		r.PerPage = request.PerPage
+	}
+
+	return r
 }
